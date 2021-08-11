4e728769360de394b3d2877acab3a553008b8f9d
mcatzin <catzin21@gmail.com>
2021-08-11 11:32:47 -0700

    Add Projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import { Layout } from "../layout/Layout";
 import Hero from "../components/Hero/Hero";
 import { Section } from "../styles/GlobalComponents";
+import Projects from "../components/Projects/Projects";
+
 const Home = () => {
   return (
     <Layout>
       <Section>
         <Hero />
       </Section>
+      <Projects />
     </Layout>
   );
 };
